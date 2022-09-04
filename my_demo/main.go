e660c0f1c8fbb6884e5974c3a6f137765b370680
qingconglaixueit <502892037@qq.com>
2022-09-04 17:46:00 +0800

    [add] add 0.2.0 chat

--- my_demo/main.go
+++ my_demo/main.go
@@ -13,6 +13,10 @@ func main() {
 	{
 		v1.GET("/query", queryInfo)
 	}
+	v2 := router.Group("/v2")
+	{
+		v2.GET("/query", queryInfo2)
+	}
 
 	router.Run(":8989")
 
@@ -24,6 +28,16 @@ func queryInfo(c *gin.Context){
 		"msg":"success",
 		"data":map[string]interface{}{
 			"info": "hello world ,v1.queryInfo",
+		},
+	})
+}
+
+func queryInfo2(c *gin.Context){
+	c.JSON(200,gin.H{
+		"code":0,
+		"msg":"success",
+		"data":map[string]interface{}{
+			"info": "hello world ,v2.queryInfo",
 		},
 	})
 }
